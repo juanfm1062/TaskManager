f6e8a026f298bc80a979027088a91c8dd39b7b6f
Juan <juanfm106@yahoo.com>
2024-03-12 21:20:53 -0700

    Refactored SecurityConfig into single class

--- src/main/java/TaskMangement/com/Task/Config/SecurityConfig.java
+++ src/main/java/TaskMangement/com/Task/Config/SecurityConfig.java
@@ -3,9 +3,11 @@ package TaskMangement.com.Task.Config;
 import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,29 +25,27 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
-@EnableMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true,
-        jsr250Enabled = true
-)
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/taskmanger/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/taskmanager/users/**").hasAnyRole("ADMIN","USER")
                         .requestMatchers("/taskmanager/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> {});
+                .httpBasic(Customizer.withDefaults())
+                .headers(headers -> {
+                    headers.contentSecurityPolicy(csp -> csp.policyDirectives("script-src 'self'"));
+                    headers.referrerPolicy(referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN));
+                    headers.permissionsPolicy(policy -> policy.policy("accelerometer=(), amient-light-sensor=()"));
+                    headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::deny);
+                    headers.contentTypeOptions(HeadersConfigurer.ContentTypeOptionsConfig::disable);
+                });
         return http.build();
     }
 
@@ -52,31 +53,17 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Authorization","Content-type"));
-        config.setExposedHeaders(Arrays.asList("Authorization","Content-type"));
+        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
+        config.setExposedHeaders(Arrays.asList("Authorization","Content-Type"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/**",config);
         return source;
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        User.UserBuilder users = User.builder().passwordEncoder(passwordEncoder()::encode);
-
-        UserDetails user = users
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = users
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
